96ca4474ce57d5f64c41984a63665d6fd000478c
Aayusha Shrestha <rosyshrestha028@gmail.com>
2022-04-06 19:04:24 +0530

    [feat] : Focus the next empty field on input changey

---
+++ src/components/form-elements/OtpInputField.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef } from "react";
+import { OTP_LENGTH } from "src/constants";
+
+interface IOTPInputFieldProps {
+    index: number
+    activeInputIndex: number
+    setActiveInputIndex: (index: number) => void
+}
+
+const OTPInputField = (props: IOTPInputFieldProps) => {
+    const { index, activeInputIndex, setActiveInputIndex } = props
+
+    const otpInputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (activeInputIndex === index) {
+            otpInputRef?.current?.focus();
+        }
+    }, [activeInputIndex, index]);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // TODO : get form values
+        handleInputFocus()
+    };
+
+    const handleInputFocus = () => {
+        if (activeInputIndex === OTP_LENGTH) {
+            setActiveInputIndex(0)
+        }
+        else {
+            setActiveInputIndex(index + 1);
+        }
+    }
+
+    return (
+        <>
+            <input
+                name={`otp-${index}`}
+                type="number"
+                ref={otpInputRef}
+                onChange={(e) => handleInputChange(e)}
+                autoFocus={false}
+                onBlur={() => setActiveInputIndex(0)}
+            />
+        </>
+    );
+};
+
+export default OTPInputField;
---
+++ src/components/otp-form/index.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { OTP_LENGTH } from "src/constants";
+import OTPInputField from "../form-elements/OtpInputField";
+
+const emptyArray = [...Array(OTP_LENGTH)];
+
+function OTPForm() {
+    const [activeInputIndex, setActiveInputIndex] = useState(0);
+    return (
+        <>
+            <h2>Verification</h2>
+            <form>
+                {
+                    emptyArray.map((_, index) =>
+                    (
+                        <OTPInputField key={index} index={index + 1} activeInputIndex={activeInputIndex} setActiveInputIndex={setActiveInputIndex} />
+                    ))
+                }
+            </form>
+        </>
+    );
+}
+
+export default OTPForm;
