f213ba1f2c0aafa9964481c518bea167564faf15
Aayusha Shrestha <rosyshrestha028@gmail.com>
2022-04-07 09:54:49 +0530

    [feat] : Allow user to paste the code from clipboard

--- src/components/form-elements/OtpInputField.tsx
+++ src/components/form-elements/OtpInputField.tsx
@@ -1,15 +1,23 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from 'react';
 
 interface IOTPInputFieldProps {
-    index: number
-    activeInputIndex: number
-    setActiveInputIndex: (index: number) => void
+    index: number;
+    activeInputIndex: number;
+    setActiveInputIndex: (index: number) => void;
+    handleOnPaste: (e: React.ClipboardEvent<HTMLInputElement>) => void;
+    defaultValue?: string | null;
 }
 
 const OTPInputField = (props: IOTPInputFieldProps) => {
-    const { index, activeInputIndex, setActiveInputIndex } = props
+    const {
+        index,
+        activeInputIndex,
+        defaultValue,
+        setActiveInputIndex,
+        handleOnPaste,
+    } = props;
 
-    const [value, setValue] = useState<string>('')
+    const [value, setValue] = useState<string>('');
 
     const otpInputRef = useRef<HTMLInputElement>(null);
 
@@ -19,17 +27,21 @@ const OTPInputField = (props: IOTPInputFieldProps) => {
         }
     }, [activeInputIndex, index]);
 
+    useEffect(() => {
+        if (defaultValue) {
+            setValue(defaultValue);
+        }
+    }, [defaultValue]);
+
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         // TODO : get form values
-
-        handleFocusOnInputChange(e.currentTarget.value)
-
+        handleFocusOnInputChange(e.currentTarget.value);
     };
 
     const handleFocus = () => {
         setActiveInputIndex(index);
         otpInputRef.current?.select();
-    }
+    };
 
     const handleFocusOnInputChange = (inputValue: string) => {
         const inputLength = inputValue.length;
@@ -39,29 +51,28 @@ const OTPInputField = (props: IOTPInputFieldProps) => {
                 setValue('');
                 break;
             case 1:
-                setValue(inputValue)
+                setValue(inputValue);
                 focusNextInput();
                 break;
             default:
                 focusNextInput();
-                return false
+                return false;
         }
-    }
+    };
 
     const focusNextInput = () => {
         setActiveInputIndex(index + 1);
-
-    }
+    };
 
     const focusPreviousInput = () => {
-        setActiveInputIndex(activeInputIndex - 1)
-    }
+        setActiveInputIndex(activeInputIndex - 1);
+    };
 
     const handleKeyDownEvent = (e: React.KeyboardEvent<HTMLInputElement>) => {
         const keys = ['+', '-', 'e', 'E', '.'];
-        const pressedKey = e.key
+        const pressedKey = e.key;
 
-        console.log(pressedKey)
+        console.log(pressedKey);
 
         if (keys.includes(pressedKey)) {
             e.preventDefault();
@@ -85,20 +96,21 @@ const OTPInputField = (props: IOTPInputFieldProps) => {
             default:
                 break;
         }
-    }
+    };
 
     return (
         <>
             <input
                 name={`otp-${index}`}
-                type="number"
+                type='number'
                 maxLength={1}
-                value={value ? value : ''}
+                value={value ?? ''}
                 ref={otpInputRef}
                 onChange={handleInputChange}
                 onFocus={handleFocus}
                 onBlur={() => setActiveInputIndex(0)}
                 onKeyDown={handleKeyDownEvent}
+                onPaste={handleOnPaste}
             />
         </>
     );
--- src/components/otp-form/index.tsx
+++ src/components/otp-form/index.tsx
@@ -1,21 +1,64 @@
-import { useState } from "react";
-import { OTP_LENGTH } from "src/constants";
-import OTPInputField from "../form-elements/OtpInputField";
+import { useState } from 'react';
+import { OTP_LENGTH } from 'src/constants';
+import OTPInputField from '../form-elements/OtpInputField';
 
 const emptyArray = [...Array(OTP_LENGTH)];
 
 function OTPForm() {
     const [activeInputIndex, setActiveInputIndex] = useState(1);
+
+    const [defaultValues, setDefaultValues] = useState<Array<string>>([]);
+
+    const handleOnPaste = (e: React.ClipboardEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const pastedValue = e.clipboardData.getData('text/plain').trim();
+        const isValid = isValidInput(pastedValue);
+
+        if (!isValid) {
+            return;
+        }
+
+        const digitsArray = pastedValue.split('', OTP_LENGTH);
+        let activeIndex = activeInputIndex - 1;
+        digitsArray.forEach((digit) => {
+            emptyArray.fill(
+                digit,
+                activeIndex,
+                activeInputIndex + digitsArray.length - 1
+            );
+            activeIndex = activeIndex + 1;
+        });
+        setDefaultValues(emptyArray);
+        setActiveInputIndex(activeIndex);
+    };
+
+    const isValidInput = (inputValue: string) => {
+        if (isNaN(+inputValue)) {
+            alert('Not a number');
+            return false;
+        }
+
+        if (inputValue.length === 0) {
+            return false;
+        }
+
+        return true;
+    };
+
     return (
         <>
             <h2>Verification</h2>
             <form>
-                {
-                    emptyArray.map((_, index) =>
-                    (
-                        <OTPInputField key={index} index={index + 1} activeInputIndex={activeInputIndex} setActiveInputIndex={setActiveInputIndex} />
-                    ))
-                }
+                {emptyArray.map((_, index) => (
+                    <OTPInputField
+                        key={index}
+                        index={index + 1}
+                        activeInputIndex={activeInputIndex}
+                        defaultValue={defaultValues[index]}
+                        setActiveInputIndex={setActiveInputIndex}
+                        handleOnPaste={handleOnPaste}
+                    />
+                ))}
             </form>
         </>
     );
